1574dca9d336d3545097954fd0650ea5985be4d5
唐丽 <tangli@yongyida.com>
2017-06-27 10:59:15 +0800

    挚康：修改默认版本号为ZK20C_V3R003

--- customer/zhikang/y20c/changeVersion.sh
+++ customer/zhikang/y20c/changeVersion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #modify  versionNumber build/tools/buildinfo.sh
 changeBuildinfo(){	
-	default_version="ZK20C_V3R002"
+	default_version="ZK20C_V3R003"
 	propPath="$TARGET_DEVICE_DIR/system.prop"
 	read -p "[autobuild.sh]:  Please input new version number (default \"$default_version\"): " VersionNumber
 	if [ -n "$VersionNumber" ];then
@@ -25,7 +25,7 @@ changeBuildinfo(){
 
 #modify new versionNumber device/.../system.prop
 changeSystemProp(){
-	default_os="ZK20C.12.002"	
+	default_os="ZK20C.12.003"	
 	
 	read -p "[autobuild.sh]:  Please input new version number (default \"$default_os\"): " SecVersionNumber
 	echo "[autobuild.sh]:  modify $propPath"
