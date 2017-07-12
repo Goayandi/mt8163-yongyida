af1567d3493d130f093762efcacc30202c61ee97
张丛 <zhangcong@yongyida.com>
2017-07-12 20:29:20 +0800

    增加优学编译选项

---
+++ customer/youxue/y20c/special_config.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+#删除系统生成的不要的apk
+
+#rm out/target/product/byt_t_ffrd10/system/app/Phone.apk
+
+#cp -rf $CUSTOMIZATION_PATH/vendor $OUT/system/
+
+rm out/target/product/yyd8163_tb_m/system/app/YYDMoliVideo/YYDMoliVideo.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotArtMuseumYYDRobotArtMuseum.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotGameCenter/YYDRobotGameCenter.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotLauncher/YYDRobotLauncher.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotLockScreen/YYDRobotLockScreen.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotPhotos/YYDRobotPhotos.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotResourceManager/YYDRobotResourceManager.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotSyn/YYDRobotSyn.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotTranslation/YYDRobotTranslation.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotVideo/YYDRobotVideo.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotVideoPlayer/YYDRobotVideoPlayer.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotVoiceCamera/YYDRobotVoiceCamera.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotVoiceNotification/YYDRobotVoiceNotification.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotWPS/YYDRobotWPS.apk
+rm out/target/product/yyd8163_tb_m/system/app/YYDRobotXMMusicSearch/YYDRobotXMMusicSearch.apk
+
+
