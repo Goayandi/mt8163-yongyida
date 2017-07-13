0bfde30fa46b8f11c1ce3d16f4c0e2e95eb41c4a
唐丽 <tangli@yydrobot.com>
2017-07-13 21:28:45 +0800

    修改妻言脚本

--- qy_auto_make.sh
+++ qy_auto_make.sh
@@ -273,7 +273,7 @@ mv "$COMMITFILE" "$VersionNumber"
  
 
 echo "copy release files to server=================>>>"
-#scp -r "$VersionNumber" peishengguo@172.16.1.246:/home/peishengguo/code/Y50D_VERSION
+scp -r "$VersionNumber" peishengguo@172.16.1.246:/home/peishengguo/code/
 
 #**************************************************************************************************
 
