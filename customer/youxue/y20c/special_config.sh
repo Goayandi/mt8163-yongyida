1692918d1d89004f14ea2c1250341e2d6f6fa101
张丛 <zhangcong@yongyida.com>
2017-07-27 20:46:36 +0800

    修改优学内核配置文件

--- customer/youxue/y20c/special_config.sh
+++ customer/youxue/y20c/special_config.sh
@@ -6,20 +6,22 @@
 
 #cp -rf $CUSTOMIZATION_PATH/vendor $OUT/system/
 
-rm out/target/product/yyd8163_tb_m/system/app/YYDMoliVideo/YYDMoliVideo.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotArtMuseumYYDRobotArtMuseum.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotGameCenter/YYDRobotGameCenter.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotLauncher/YYDRobotLauncher.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotLockScreen/YYDRobotLockScreen.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotPhotos/YYDRobotPhotos.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotResourceManager/YYDRobotResourceManager.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotSyn/YYDRobotSyn.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotTranslation/YYDRobotTranslation.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotVideo/YYDRobotVideo.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotVideoPlayer/YYDRobotVideoPlayer.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotVoiceCamera/YYDRobotVoiceCamera.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotVoiceNotification/YYDRobotVoiceNotification.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotWPS/YYDRobotWPS.apk
-rm out/target/product/yyd8163_tb_m/system/app/YYDRobotXMMusicSearch/YYDRobotXMMusicSearch.apk
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDMoliVideo
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotArtMuseum
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotGameCenter
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotLauncher
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotFloatButton
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotLockScreen
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotPhotos
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotResourceManager
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotSyn
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotTranslation
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotVideo
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotVideoPlayer
+#rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotVoiceCamera
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotVoiceNotification
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotWPS
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotXMMusicSearch
+
 
 
