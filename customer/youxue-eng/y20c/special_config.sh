dadb02f369bbc17b0081475179302a6725ca88ae
peishengguo <peishengguo@yydrobot.com>
2017-09-14 11:12:08 +0800

    submit

---
+++ customer/youxue-eng/y20c/special_config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#删除系统生成的不要的apk
+
+#rm out/target/product/byt_t_ffrd10/system/app/Phone.apk
+
+#cp -rf $CUSTOMIZATION_PATH/vendor $OUT/system/
+
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDMoliVideo
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotArtMuseum
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotGameCenter
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotLauncher
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotFloatButton
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotLockScreen
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotPhotos
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotResourceManager
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotSyn
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotTranslation
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotVideo
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotVideoPlayer
+#rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotVoiceCamera
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotVoiceNotification
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotWPS
+rm -rf out/target/product/yyd8163_tb_m/system/app/YYDRobotXMMusicSearch
+
+
+
