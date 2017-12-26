715607385c4759f745517a39435c74188a446768
peishengguo <peishengguo@yydrobo.com>
2017-12-26 10:09:49 +0800

    modify y20d

--- customer/xiaoyong-eng/y20d/changeVersion.sh
+++ customer/xiaoyong-eng/y20d/changeVersion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #modify  versionNumber build/tools/buildinfo.sh
 changeBuildinfo(){	
-	default_version="YOS3.0_Y20CPRO_V1R001"
+	default_version="YOS3.0_Y20D_V1R001"
 	propPath="$TARGET_DEVICE_DIR/system.prop"
 	ProjectConfigPath="$TARGET_DEVICE_DIR/ProjectConfig.mk"
 	read -p "[autobuild.sh]:  Please input new version number (default \"$default_version\"): " VersionNumber
