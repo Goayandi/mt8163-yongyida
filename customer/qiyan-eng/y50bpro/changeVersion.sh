5f0a94d885dbac000b251f0c3a2bb6a047cc9ac5
peishengguo <peishengguo@yydrobo.com>
2017-12-27 15:37:24 +0800

    modify qiayn version

--- customer/qiyan-eng/y50bpro/changeVersion.sh
+++ customer/qiyan-eng/y50bpro/changeVersion.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #modify  versionNumber build/tools/buildinfo.sh
 changeBuildinfo(){	
-	default_version="QIYAN_Y50E_V1R004_test"
+	default_version="QIYAN_Y50E_V1R005"
 	propPath="$TARGET_DEVICE_DIR/system.prop"
 	ProjectConfigPath="$TARGET_DEVICE_DIR/ProjectConfig.mk"
 	read -p "[autobuild.sh]:  Please input new version number (default \"$default_version\"): " VersionNumber
